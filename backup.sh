9d266d2b4f5af0b0c3f8ea81bc978efacb267e2b
michaelcurran <mcurran@articulate.com>
2016-11-07 23:18:16 -0800

    PSQL backup script

---
+++ backup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Script designed to work with AWS Data Pipeline
+# which automates backing up and testing psql backups
+
+export AWS_DEFAULT_REGION=us-east-1
+export PGPASSWORD=$RDS_PASSWORD
+export PSQL_TOOLS_VERSION=$(echo $PSQL_VERSION | awk -F\. '{print $1$2}')
+
+dump_file=$APP_NAME-$(date +%Y_%m_%d_%H%M%S).sql
+
+# Install the postgres tools matching the engine version
+sudo yum install -y postgresql$PSQL_TOOLS_VERSION
+
+# Take the backup
+pg_dump -Fc -h $RDS_ENDPOINT -U $RDS_USERNAME -d $DATABASE_NAME > $dump_file
+
+# Upload it to s3
+aws s3 cp $dump_file s3://$S3_BUCKET/$APP_NAME/
+
+# Delete the file
+rm $dump_file
+
+# Grab it from s3 to make sure it's intact
+aws s3 cp s3://$S3_BUCKET/$APP_NAME/$dump_file .
+
+# Create SQL script and remove extension comments
+pg_restore $dump_file > restore.sql
+sed -i '/COMMENT ON EXTENSION/d' restore.sql
+
+# Create the RDS restore instance
+aws rds create-db-instance --db-name $DATABASE_NAME --db-instance-identifier postgres-$APP_NAME-auto-restore --db-instance-class $INSTANCE_SIZE --engine postgres --master-username $RDS_USERNAME --master-user-password $RDS_PASSWORD --vpc-security-group-ids $SECURITY_GROUP_ID --no-multi-az --storage-type gp2 --allocated-storage $STORAGE_SIZE --engine-version $PSQL_VERSION --no-publicly-accessible --db-subnet-group $SUBNET_GROUP_NAME --backup-retention-period 0
+
+# Wait for the rds endpoint to be available before setting it
+while [[ ! $(aws rds describe-db-instances --db-instance-identifier postgres-$APP_NAME-auto-restore --output=text | awk '/ENDPOINT/ {print $2}') =~ "rds.amazonaws.com" ]]
+do
+  sleep 30s
+done
+
+restore_endpoint=$(aws rds describe-db-instances --db-instance-identifier postgres-$APP_NAME-auto-restore --output=text | awk '/ENDPOINT/ {print $2}')
+
+# Restore the data
+psql --set ON_ERROR_STOP=on -h $restore_endpoint -U $RDS_USERNAME -d $DATABASE_NAME < restore.sql
+
+if [ "$?" == "0" ]
+then
+  aws rds delete-db-instance --db-instance-identifier postgres-$APP_NAME-auto-restore --skip-final-snapshot
+  if [ "$?" == "0" ]
+  then
+    # Notify DMS if successful
+    curl $DMS_URL
+  fi
+else
+  aws rds delete-db-instance --db-instance-identifier postgres-$APP_NAME-auto-restore --skip-final-snapshot
+fi
