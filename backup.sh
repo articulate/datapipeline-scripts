723a5901050219eb276564d4a5f86d033db88ac5
Brian Stolz <brian@tecnobrat.com>
2017-08-15 11:27:48 -0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -15,8 +15,15 @@ if [ "$DB_ENGINE" == "sqlserver-se" ]; then
   sudo service docker start
   sleep 30
 
-  TASK_ID=$(sudo docker run -it microsoft/mssql-server-linux /opt/mssql-tools/bin/sqlcmd -S $RDS_ENDPOINT -U $RDS_USERNAME -P $RDS_PASSWORD -Q "exec msdb.dbo.rds_backup_database @source_db_name='$DB_NAME', @s3_arn_to_backup_to='arn:aws:s3:::$BACKUP_BUCKET/$SERVICE_NAME/$DUMP_FILE', @overwrite_S3_backup_file=1;" -W -s ',' -k 1 |head -3 | csvcut -c 'task_id' | tail -1)
-  echo "Started mssql backup with TASK_ID=${TASK_ID}"
+  TASK_OUTPUT=$(sudo docker run -it microsoft/mssql-server-linux /opt/mssql-tools/bin/sqlcmd -S $RDS_ENDPOINT -U $RDS_USERNAME -P $RDS_PASSWORD -Q "exec msdb.dbo.rds_backup_database @source_db_name='$DB_NAME', @s3_arn_to_backup_to='arn:aws:s3:::$BACKUP_BUCKET/$SERVICE_NAME/$DUMP_FILE', @overwrite_S3_backup_file=1;" -W -s ',' -k 1)
+  if [ "$?" == "0" ]; then
+    TASK_ID=$(echo $TASK_OUTPUT | head -3 | csvcut -c 'task_id' | tail -1)
+    echo "Started mssql backup with TASK_ID=${TASK_ID}"
+  else
+    echo "MSSQL task could not be started"
+    echo $TASK_OUTPUT
+    exit 1
+  fi
 else
   PSQL_TOOLS_VERSION=$(echo $DB_ENGINE_VERSION | awk -F\. '{print $1$2}')
   DUMP_FILE=$SERVICE_NAME-$(date +%Y_%m_%d_%H%M%S).sql
