8cabc884c449652faa2d02ef0556fc0ac9b6db73
Melissa Meyer <mmeyer@articulate.com>
2017-09-26 11:32:54 -0700

    mssql server backup/restore use kms key to encrypt
    https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/SQLServer.Procedural.Importing.html
    add @kms_master_key_arn option to msdb.dbo.rds.(backup|restore) commands
    backup terraform module adds a distinct kms key per backup pipeline

--- backup.sh
+++ backup.sh
@@ -15,7 +15,7 @@ if [ "$DB_ENGINE" == "sqlserver-se" ]; then
   sudo service docker start
   sleep 30
 
-  TASK_OUTPUT=$(sudo docker run -t microsoft/mssql-server-linux /opt/mssql-tools/bin/sqlcmd -S $RDS_ENDPOINT -U $RDS_USERNAME -P $RDS_PASSWORD -Q "exec msdb.dbo.rds_backup_database @source_db_name='$DB_NAME', @s3_arn_to_backup_to='arn:aws:s3:::$BACKUP_BUCKET/$SERVICE_NAME/$DUMP_FILE', @overwrite_S3_backup_file=1;" -W -s ',' -k 1)
+  TASK_OUTPUT=$(sudo docker run -t microsoft/mssql-server-linux /opt/mssql-tools/bin/sqlcmd -S $RDS_ENDPOINT -U $RDS_USERNAME -P $RDS_PASSWORD -Q "exec msdb.dbo.rds_backup_database @source_db_name='$DB_NAME', @kms_master_key_arn='$KMS_KEY', @s3_arn_to_backup_to='arn:aws:s3:::$BACKUP_BUCKET/$SERVICE_NAME/$DUMP_FILE', @overwrite_S3_backup_file=1;" -W -s ',' -k 1)
   TASK_ID=$(echo "$TASK_OUTPUT" | head -3 | csvcut -c 'task_id' | tail -1)
 
   if [[ "$?" == "0" && -n "$TASK_ID" ]]; then
@@ -103,7 +103,7 @@ if [ "$SUCCESS" == "0" ]; then
       sleep 30s
     done
 
-    RESTORE_TASK_ID=$(sudo docker run -t microsoft/mssql-server-linux /opt/mssql-tools/bin/sqlcmd -S $RESTORE_ENDPOINT -U $RDS_USERNAME -P $RDS_PASSWORD -Q "exec msdb.dbo.rds_restore_database @restore_db_name='$DB_NAME', @s3_arn_to_restore_from='arn:aws:s3:::$BACKUP_BUCKET/$SERVICE_NAME/$DUMP_FILE';" -W -s "," -k 1 |head -3 | csvcut -c "task_id" | tail -1)
+    RESTORE_TASK_ID=$(sudo docker run -t microsoft/mssql-server-linux /opt/mssql-tools/bin/sqlcmd -S $RESTORE_ENDPOINT -U $RDS_USERNAME -P $RDS_PASSWORD -Q "exec msdb.dbo.rds_restore_database @restore_db_name='$DB_NAME', @kms_master_key_arn='$KMS_KEY', @s3_arn_to_restore_from='arn:aws:s3:::$BACKUP_BUCKET/$SERVICE_NAME/$DUMP_FILE';" -W -s "," -k 1 |head -3 | csvcut -c "task_id" | tail -1)
     RESTORE_TASK_STATUS=$(sudo docker run -t microsoft/mssql-server-linux /opt/mssql-tools/bin/sqlcmd -S $RESTORE_ENDPOINT -U $RDS_USERNAME -P $RDS_PASSWORD -Q "exec msdb.dbo.rds_task_status @task_id='$RESTORE_TASK_ID'" -W -s "," -k 1 | csvcut -c "lifecycle" | tail -1)
     while [[ "$RESTORE_TASK_STATUS" == "CREATED" || $RESTORE_TASK_STATUS = "IN_PROGRESS" ]]; do
       echo "Status is still $RESTORE_TASK_STATUS"
