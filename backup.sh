6269294578d5f05008b385761ce276c2824f95b7
Sarah Riley <sarah.riley@articulate.com>
2021-03-13 18:19:54 -0700

    With the new python 3 and the new csvkit, the inputs to csvcut need to be modified like this to give the expected output

--- backup.sh
+++ backup.sh
@@ -138,7 +138,7 @@ if [[ $DB_ENGINE == "sqlserver-se" ]]; then
 
   # Get the task id of the backup task status
   echo "Get the task id..."
-  TASK_ID=$(echo "$TASK_OUTPUT" | csvcut -c "task_id" | grep "Task Id" | grep -o "[0-9]*")
+  TASK_ID=$(echo "$TASK_OUTPUT" | sed -e "s/\r/\n/g" | csvcut -c "task_id" | grep "Task Id" | grep -o "[0-9]*")
   if [[ -n $TASK_ID ]]; then
     echo "Started mssql backup with task id: $TASK_ID"
   else
@@ -151,7 +151,7 @@ if [[ $DB_ENGINE == "sqlserver-se" ]]; then
   function backup_task_status {
     sqlcmd_with_backoff $SQLCMD -S $RDS_ENDPOINT -U $RDS_USERNAME -P $RDS_PASSWORD -Q \
     "exec msdb.dbo.rds_task_status @task_id='$TASK_ID'" -W -s "," -k 1 \
-    | csvcut -c "lifecycle" | tail -1
+    | sed -e "s/\r/\n/g" | csvcut -c "lifecycle" | tail -1
   }
 
   BACKUP_TASK_STATUS=$(backup_task_status)
