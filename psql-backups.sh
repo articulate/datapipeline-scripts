2941ace6c5088c40852ad7e75a87414cd93d8a69
Duck Lawn <duckalini@gmail.com>
2022-02-22 13:13:41 +1300

    Remove compression from pg_dump command

--- psql-backups.sh
+++ psql-backups.sh
@@ -105,7 +105,7 @@ _log "Taking the backup..."
 
 export PGPASSWORD=$RDS_PASSWORD
 if [[ "$majorVersion" == "9" ]]; then
-  pg_dump -Fc -j 4 -h "$RDS_ENDPOINT" -U "$RDS_USERNAME" -d "$DB_NAME" -f "$DUMP_FILE" -N apgcc
+  pg_dump -Fd -j 4 -h "$RDS_ENDPOINT" -U "$RDS_USERNAME" -d "$DB_NAME" -f "$DUMP_FILE" -N apgcc
 else
   pg_dumpall --globals-only -U "$RDS_USERNAME" -h "$RDS_ENDPOINT" -f "$DUMP_FILE"
 fi
